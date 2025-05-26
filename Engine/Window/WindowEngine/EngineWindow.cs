eba4831cabb9104f2ae127425bcfb6a93df57c77
Sasha5685 <sasahrustalev68@gmail.com>
2025-05-26 20:15:35 +0300

    FirstWindow

---
+++ Engine/Window/WindowEngine/EngineWindow.cs
@@ -0,0 +1,29 @@
+﻿using OpenTK;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+namespace UtegisEngine
+{
+    public class EngineWindow : GameWindow
+    {
+        public EngineWindow()
+            : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+        {
+            this.CenterWindow(new Vector2i(1280, 768));
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.ClearColor(new Color4(0.1f,0.5f, 0.9f, 1f));
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+             
+            this.Context.SwapBuffers();
+            base.OnRenderFrame(args);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UtegisEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (EngineWindow EngineWindow = new EngineWindow())
+            {
+                EngineWindow.Run();
+            }
+        }
+    }
+}
